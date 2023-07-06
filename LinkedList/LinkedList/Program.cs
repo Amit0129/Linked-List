e9b5a3e579324c599a6dec30aa22f85fce97d2a7
Amit Kumar Nayak <amitkumarnayak40@gmail.com>
2023-07-06 20:36:22 +0530

    [Amit]Add : Cresting a simple Linked List

---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+            foreach (var item in linkedList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
